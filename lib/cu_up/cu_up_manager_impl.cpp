6c22f01a260987b1bed0e3ac30c634d390ad4c45
Nikolai Mareev <nikolai.mareev@htwk-leipzig.de>
2025-08-28 14:17:47 +0000

    headache #1

--- lib/cu_up/cu_up_manager_impl.cpp
+++ lib/cu_up/cu_up_manager_impl.cpp
@@ -87,6 +87,7 @@ void cu_up_manager_impl::schedule_ue_async_task(ue_index_t ue_index, async_task<
 e1ap_bearer_context_setup_response
 cu_up_manager_impl::handle_bearer_context_setup_request(const e1ap_bearer_context_setup_request& msg)
 {
+    logger.debug("PIP cu_up_manager_impl::handle_bearer_context_setup_request(const e1ap_bearer_context_setup_request& msg)");
   e1ap_bearer_context_setup_response response = {};
   response.ue_index                           = INVALID_UE_INDEX;
   response.success                            = false;
