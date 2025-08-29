5c39f09865499fa2483a0234a1569a13b9b2adf0
Nikolai Mareev <nikolai.mareev@htwk-leipzig.de>
2025-08-29 05:46:30 +0000

    plain_ip_dl_adapter_ test

--- lib/cu_up/pdu_session_manager_impl.cpp
+++ lib/cu_up/pdu_session_manager_impl.cpp
@@ -194,12 +194,12 @@ new_session->plain_ip_adapter_->register_rx_notifier("192.168.100.10", *new_sess
 new_session->plain_ip_dl_adapter_->connect_plain_ip(*new_session->plain_ip_adapter_);
 new_session->sdap_to_gtpu_adapter.connect_plain_ip_dl(*new_session->plain_ip_dl_adapter_);
     }
-//}else{
-//// Connect adapters
-////ul
-//  new_session->sdap_to_gtpu_adapter.connect_gtpu(*new_session->gtpu->get_tx_lower_layer_interface());
-//}
+else{
+// Connect adapters
+//ul
   new_session->sdap_to_gtpu_adapter.connect_gtpu(*new_session->gtpu->get_tx_lower_layer_interface());
+}
+  //new_session->sdap_to_gtpu_adapter.connect_gtpu(*new_session->gtpu->get_tx_lower_layer_interface());
 
 //dl
   new_session->gtpu_to_sdap_adapter.connect_sdap(new_session->sdap->get_sdap_tx_sdu_handler());
